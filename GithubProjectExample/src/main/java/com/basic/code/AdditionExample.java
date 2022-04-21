54b6a22d1e4a220c082ab84e5bce2c7e760c4313
sharma0102 <73307520+sharma0102@users.noreply.github.com>
2022-04-21 10:45:37 +0530

    Demo Project

---
+++ GithubProjectExample/src/main/java/com/basic/code/AdditionExample.java
@@ -0,0 +1,13 @@
+package com.basic.code;
+
+public class AdditionExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a,b,c;
+		a=10;
+		b=5;
+		System.out.println(c=a+b);
+	}
+
+}
